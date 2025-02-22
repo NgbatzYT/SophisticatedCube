39ceaa8966440ddf598861a91d6cccd81668f2d1
NgbatzYT <94680235+NgbatzYT@users.noreply.github.com>
2025-02-22 20:39:24 +1100

    Add files via upload

--- Plugin.cs
+++ Plugin.cs
@@ -7,29 +7,35 @@ using UnityEngine.Animations.Rigging;
 
 namespace SophisticatedCube
 {
-    [BepInPlugin("com.ngbatz.gorillatag.sophisticatedcube", "SophisticatedCube", "1.0.1")]
+    [BepInPlugin("com.ngbatz.gorillatag.sophisticatedcube", "SophisticatedCube", "2.0.0")]
 	public class Plugin : BaseUnityPlugin
 	{
 		public static Plugin instance;
-        public static AssetBundle bundle;
+        //public static AssetBundle bundle;
         public static GameObject assetBundleParent;
-        public static string assetBundleName = "grabbbbb";
-        public static string parentName = "BundleParent (put objects in here DONT MOVE)";
+        Material material = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+        /*public static string assetBundleName = "grabbbbb";
+        public static string parentName = "BundleParent (put objects in here DONT MOVE)";*/
 
-		public void Start()
+        public void Start()
 		{
 			instance = this;
-			bundle = LoadAssetBundle("SophisticatedCube.AssetBundles." + assetBundleName);
-            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
+			/*bundle = LoadAssetBundle("SophisticatedCube.AssetBundles." + assetBundleName);
+            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));*/
+            assetBundleParent = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            assetBundleParent.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            material.SetColor("_BaseColor", Color.green);
+            assetBundleParent.GetComponent<Renderer>().material = material;
+            assetBundleParent.GetComponent<BoxCollider>().enabled = false;
             assetBundleParent.transform.position = new Vector3(-67.2225f, 11.57f, -82.611f);
         }
 
-        public AssetBundle LoadAssetBundle(string path)
+        /*public AssetBundle LoadAssetBundle(string path)
         {
             Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
             AssetBundle bundle = AssetBundle.LoadFromStream(stream);
             stream.Close();
             return bundle;
-        }
+        }*/
     }
 }
